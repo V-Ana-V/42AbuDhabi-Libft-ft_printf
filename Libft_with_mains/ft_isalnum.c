8d52b9106092bd226a973ddefcc7cc658fea2e9b
Anastasiia <95747112+V-Ana-V@users.noreply.github.com>
2024-05-06 11:26:59 +0400

    cleaned

--- Libft_with_mains/ft_isalnum.c
+++
@@ -1,6 +0,0 @@
-int ft_isalnum(int n)
-{
-	if (ft_isalpha(n) == 1 || ft_isdigit == 1)
-		return (1);
-	return (0);
-}
--- Libft_with_mains/ft_isprint.c
+++
@@ -1,6 +0,0 @@
-int	ft_isprint(int n)
-{
-	if (n >= 32 && n <= 126)
-		return (1);
-	return (0);
-}
